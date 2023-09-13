d84381fb5e51e8a5b033d2e5f40268cf9deb669a
ostoaleonardo <ostoaleonardo.dev@gmail.com>
2023-09-12 21:58:55 -0600

    Fix design track and artist card

--- src/components/ArtistCard.jsx
+++ src/components/ArtistCard.jsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom'
-import { Card, CardHeader, CardFooter, Image, Button } from '@nextui-org/react'
+import { Card, CardHeader, CardBody, CardFooter, Image, Button } from '@nextui-org/react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faArrowUpRightFromSquare } from '@fortawesome/free-solid-svg-icons'
 
@@ -8,11 +8,12 @@ export function ArtistCard({ index, artist }) {
         <Link to={'/artist/' + artist.id}>
             <Card
                 radius='lg'
-                className='col-span-1 border-none'
+                shadow='none'
+                className='col-span-1'
             >
-                <CardHeader className='absolute justify-between pr-0 py-1'>
+                <CardHeader className='absolute justify-between ml-1 pr-2 py-1'>
                     <span className='text-lg font-bold drop-shadow'>
-                        {'#' + index}
+                        {index}.
                     </span>
                     <Button
                         isIconOnly
@@ -24,7 +25,7 @@ export function ArtistCard({ index, artist }) {
                         <FontAwesomeIcon icon={faArrowUpRightFromSquare} />
                     </Button>
                 </CardHeader>
-                <div className='w-auto h-full'>
+                <CardBody className='p-0'>
                     <Image
                         isZoomed
                         width='100%'
@@ -33,11 +34,11 @@ export function ArtistCard({ index, artist }) {
                         src={artist.image}
                         className='object-cover aspect-square z-0'
                     />
-                    <div className='absolute top-0 bottom-0 left-0 right-0 bg-gradient-to-t from-transparent from-80% via-black/30 to-black/50' />
-                    <div className='absolute top-0 bottom-0 left-0 right-0 bg-gradient-to-b from-transparent from-60% via-black/30 to-black/50' />
-                </div>
-                <CardFooter className='flex overflow-hidden py-1 absolute bottom-1 w-[calc(100%_-_8px)] ml-1 z-10'>
-                    <p className='text-xs sm:text-lg font-bold drop-shadow'>
+                    <div className='absolute w-full h-full bg-gradient-to-t from-transparent from-80% via-black/30 to-black/50' />
+                    <div className='absolute w-full h-full bg-gradient-to-b from-transparent from-70% via-black/30 to-black/50' />
+                </CardBody>
+                <CardFooter className='absolute flex bottom-1 ml-1 py-2 z-10'>
+                    <p className='text-sm sm:text-lg font-semibold line-clamp-2'>
                         {artist.name}
                     </p>
                 </CardFooter>
--- src/components/TrackCard.jsx
+++ src/components/TrackCard.jsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom'
-import { Card, CardHeader, CardFooter, Image, Button } from '@nextui-org/react'
+import { Card, CardHeader, CardBody, CardFooter, Image, Button } from '@nextui-org/react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPlay } from '@fortawesome/free-solid-svg-icons'
 
@@ -14,24 +14,24 @@ export function TrackCard({ index, track }) {
         <Link to={'/track/' + track.id}>
             <Card
                 radius='lg'
-                className='col-span-1 border-none'
+                shadow='none'
+                className='col-span-1'
             >
-                <CardHeader className='absolute justify-between pr-0 py-1'>
+                <CardHeader className='absolute justify-between ml-1 py-2 pr-2'>
                     <span className='text-lg font-bold drop-shadow'>
-                        {'#' + index}
+                        {index}.
                     </span>
-                    {track.previewUrl !== null && (
-                        <Button
-                            isIconOnly
-                            onClick={() => playPreview(track.previewUrl)}
-                            variant='light'
-                            showAnchorIcon
-                        >
-                            <FontAwesomeIcon icon={faPlay} />
-                        </Button>
-                    )}
+                    <Button
+                        isIconOnly
+                        onClick={() => playPreview(track.previewUrl)}
+                        variant='light'
+                        showAnchorIcon
+                        className={track.previewUrl === null ? 'invisible' : ''}
+                    >
+                        <FontAwesomeIcon icon={faPlay} />
+                    </Button>
                 </CardHeader>
-                <div className='w-auto h-full'>
+                <CardBody className='p-0'>
                     <Image
                         isZoomed
                         width='100%'
@@ -40,16 +40,16 @@ export function TrackCard({ index, track }) {
                         src={track.albumImageUrl}
                         className='object-cover aspect-square z-0'
                     />
-                    <div className='absolute top-0 bottom-0 left-0 right-0 bg-gradient-to-t from-transparent from-80% via-black/30 to-black/50' />
-                    <div className='absolute top-0 bottom-0 left-0 right-0 bg-gradient-to-b from-transparent from-60% via-black/30 to-black/50' />
-                </div>
-                <CardFooter className='flex flex-col items-start overflow-hidden py-1 absolute bottom-1 w-[calc(100%_-_8px)] ml-1 z-10'>
-                    <p className='text-sm font-bold drop-shadow line-clamp-1'>
+                    <div className='absolute w-full h-full bg-gradient-to-t from-transparent from-80% via-black/30 to-black/50' />
+                    <div className='absolute w-full h-full bg-gradient-to-b from-transparent from-60% via-black/30 to-black/50' />
+                </CardBody>
+                <CardFooter className='absolute w-[calc(100%_-_8px)] flex flex-col items-start bottom-1 ml-1 py-2 z-10'>
+                    <span className='text-xs sm:text-sm font-semibold drop-shadow line-clamp-1'>
                         {track.artist}
-                    </p>
-                    <p className='text-xs sm:text-lg font-bold drop-shadow line-clamp-1'>
+                    </span>
+                    <span className='text-sm sm:text-lg font-semibold drop-shadow line-clamp-1'>
                         {track.title}
-                    </p>
+                    </span>
                 </CardFooter>
             </Card>
         </Link>
