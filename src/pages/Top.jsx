ed3e1c1ab56b6eed5cf43b5f9e246e85d1766936
ostoaleonardo <ostoaleonardo.dev@gmail.com>
2023-09-23 11:49:55 -0600

    Change tabs color and variant

--- src/pages/Top.jsx
+++ src/pages/Top.jsx
@@ -7,11 +7,11 @@ const Top = () => {
         <main className='min-h-screen flex flex-col items-center p-6'>
             <div className='w-full grid grid-cols-3 sm:grid-cols-4 gap-2 md:gap-4 max-w-5xl md:max-w-5xl xl:max-w-7xl'>
                 <div className='col-span-4'>
-                    <Tabs aria-label='Options'>
-                        <Tab key='photos' title='Songs'>
+                    <Tabs aria-label='Options' color='success' variant='underlined' className='w-full font-semibold'>
+                        <Tab key='photos' title='Songs' className='text-base'>
                             <TopTracks />
                         </Tab>
-                        <Tab key='music' title='Artists'>
+                        <Tab key='music' title='Artists' className='text-base'>
                             <TopArtists />
                         </Tab>
                     </Tabs>
