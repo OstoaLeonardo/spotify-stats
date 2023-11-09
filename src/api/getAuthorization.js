693f382060c6a90fa7452f7a93aaf1407ace5cd9
ostoaleonardo <ostoaleonardo.dev@gmail.com>
2023-11-09 13:47:47 -0600

    Change redirect uri

--- src/api/getAuthorization.js
+++ src/api/getAuthorization.js
@@ -1,7 +1,7 @@
 const authEndpoint = 'https://accounts.spotify.com/authorize';
 const clientId = 'b98830dfd88f4b03b153bbe48623e883';
 const responseType = 'code';
-const redirectUri = 'http://localhost:5173/';
+const redirectUri = 'https://wavestify.vercel.app/';
 const scopes = [
     'user-read-email',
     'user-read-private',
