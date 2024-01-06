ba3a690a57fa1e857fd29a66d84b1b1d53050b7d
ostoaleonardo <ostoaleonardo.dev@gmail.com>
2024-01-05 19:51:02 -0600

    Set redirect uri based on mode

--- src/constants/spotify.js
+++ src/constants/spotify.js
@@ -1,3 +1,7 @@
+const MODE = import.meta.env.MODE;
+const DEV_URL = 'http://localhost:5173/';
+const PROD_URL = 'https://wavestify.vercel.app/';
+
 const SCOPES = [
     'user-read-email',
     'user-read-private',
@@ -9,7 +13,7 @@ const SCOPES = [
 ];
 
 export const SPOTIFY = {
-    REDIRECT_URI: 'http://localhost:5173/',
+    REDIRECT_URI: MODE !== 'development' ? PROD_URL : DEV_URL,
     AUTH_ENDPOINT: 'https://accounts.spotify.com/authorize',
     TOKEN_ENDPOINT: 'https://accounts.spotify.com/api/token',
     SCOPES: SCOPES,
